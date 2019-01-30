1d9aba55b00c70084692f113065ff9efcc2e00bc
Eric Daniel <ericdaniel2272@gmail.com>
2019-01-30 17:25:50 +0100

    Etape remote github

---
+++ src/Migrations/Version20190130162409.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190130162409 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE facture (id INT AUTO_INCREMENT NOT NULL, abonnes_id INT NOT NULL, numero_facture INT NOT NULL, date_edition DATE NOT NULL, date_presentation DATE NOT NULL, date_limite_paiement DATE NOT NULL, consommation_totale INT NOT NULL, net_apayer INT NOT NULL, nouvel_index INT NOT NULL, INDEX IDX_FE866410FEDEAEF2 (abonnes_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE facture ADD CONSTRAINT FK_FE866410FEDEAEF2 FOREIGN KEY (abonnes_id) REFERENCES abonne (id)');
+        $this->addSql('DROP TABLE releve');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE releve (id INT AUTO_INCREMENT NOT NULL, nouvel_index INT NOT NULL, date_releve DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE facture');
+    }
+}
